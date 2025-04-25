041c13d7b2e6139b4c5b776db63897c78969fedf
Didier DRACHE <didier.drache@gmail.com>
2025-04-25 09:46:58 +0200

    Correction du workflow - ajout initialisation du modele

--- application.py
+++ application.py
@@ -1,3 +1,7 @@
 # application.py
 
-from api import app
+from api import app, load_model_and_resources
+
+# Initialiser le modèle
+import api
+api.model, api.tokenizer, api.model_type, api.current_config = load_model_and_resources()
